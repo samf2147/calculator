e3ed55cb4b1289ad74dbbe86495b2b7f665f05e6
Sam Frank <sfrank2147@gmail.com>
2013-12-30 15:35:35 -0600

    Catches errors

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 import sys
+from optparse import OptionParser
 
 from tip_calculator_as_functions import calculate_rate, calculate_meal_costs
 
@@ -6,7 +7,7 @@ def valid_meal_base(price):
     '''Return True if price is a valid value for the meal'''
     try:
         price = float(price)
-    except ValueError:
+    except (ValueError, TypeError):
         return False
     if price < 0:
         return False
@@ -16,7 +17,7 @@ def valid_percentage(rate):
     '''Return True if rate is a valid percentage between 0 and 100'''
     try:
         rate = float(rate)
-    except ValueError:
+    except (ValueError, TypeError):
         return False
     if rate < 0 or rate > 100:
         return False
@@ -39,18 +40,49 @@ def get_user_value(condition, message, error_message):
             print error_message
     return user_input
             
+def main():
+    ''' Calculate costs for a meal
     
+    The user inputs meal cost, tax rate, and tip rate as parameters
+    If any parameters are invalid, the program will
+    ask the user for valid ones.
+    '''
     
+    parser = OptionParser()
+    parser.add_option("-m", "--meal", dest="meal", help="base cost of meal")
+    parser.add_option("-x", "--tax", dest="tax_percent")
+    parser.add_option("-t", "--tip", dest="tip_percent", 
+                        help="percent tip you want to leave", default=20)  
+                        #let's accrue good karma by defaulting to a decent 20%
+                        #tip!
+    (options, args) = parser.parse_args()
 
-# def get_meal_base():
-#     '''get base price of meal using raw input'''
-#     while True:
-#         try:
-#             meal_base = float(raw_input('Enter base price for meal in dollars: '))
-#             if meal_base < 0.0:
-#                 raise ValueError
-#             break
-#         except ValueError:
-#             print 'Please enter a positive number'
-#     return meal_base
+    #make sure we get valid values
+    if not valid_meal_base(options.meal):
+        options.meal = get_user_value(valid_meal_base, 
+                        'Please enter a valid meal price: ', 
+                        'That is not a positive number')
+    meal = float(options.meal)
+    
+    if not valid_percentage(options.tax_percent):
+        options.tax_percent = get_user_value(valid_percentage,
+                                  'Please enter a valid tax percentage: ',
+                                  'Enter a positive number between 0 and 100')
+    tax_percent = float(options.tax_percent)
+    
+    if not valid_percentage(options.tip_percent):
+        options.tip_percent = get_user_value(valid_percentage,
+                                  'Please enter a valid tip percentage: ',
+                                  'Enter a positive number between 0 and 100')
+    tip_percent = float(options.tip_percent)
+    
+    costs = calculate_meal_costs(meal, 0.01*tax_percent, 0.01*tip_percent)
+    meal_plus_tax = calculate_rate(0.01*tax_percent, meal) + meal
+    tip_amount = costs['total'] - meal_plus_tax
+    
+    print 'The meal plus tax costs {:.2f} dollars'.format(meal_plus_tax)
+    print 'You should tip {:.2f} dollars.'.format(tip_amount)
 
+if __name__ == '__main__':
+    main()
+                                         
